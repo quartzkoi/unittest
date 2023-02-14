14b4f6a95b2aa90ccf715e2186564c72114fd892
quartzkoi <quartzkoi@gmail.com>
2023-02-14 10:19:30 -0600

    Final Commit

--- student_test.py
+++ student_test.py
@@ -1,26 +1,26 @@
 import unittest
 from Student import Student, Event, Assignment
 
-class TestAssignment(unittest.TestCase):
+class TestAssignment(unittest.TestCase): #Confirming if grade percentages are correct
     def test_getPercentage(self):
         a = Assignment("Assignment 1", 10, 20)
         self.assertEqual(a.getPercentage(), 0.5)
 
-class TestStudent(unittest.TestCase):
+class TestStudent(unittest.TestCase):#Confirming correct student
     def setUp(self):
         self.student = Student("Matthew Pogue", "h735f787")
 
-    def test_addEvent(self):
+    def test_addEvent(self):#Testing if Events are properly added
         event = Event("Lecture", "Meeting")
         self.student.addEvent(event)
         self.assertEqual(len(self.student.events), 1)
 
-    def test_countMeetings(self):
+    def test_countMeetings(self): #Testing the number and type of each event
         events = [Event("Lecture 1", "Meeting"), Event("Lecture 2", "Meeting")]
         self.student.addEvent(events)
         self.assertEqual(self.student.countMeetings(), 3)
 
-    def test_getGrade(self):
+    def test_getGrade(self): #Testing the numerical grade of each assignment
         assignments = [Assignment("Assignment 1", 10, 20), Assignment("Assignment 2", 20, 20)]
         self.student.addAssignment(assignments)
         self.assertEqual(self.student.getGrade(), 0.75)
